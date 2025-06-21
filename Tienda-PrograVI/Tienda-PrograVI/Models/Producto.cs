631252217663f3a22ead5b4fce380b1891467bb7
Juan Carlos Nuñez Chavarria <134462352+JuankChavarria@users.noreply.github.com>
2025-06-21 17:52:49 -0600

    Inicio producto

---
+++ Tienda-PrograVI/Tienda-PrograVI/Models/Producto.cs
@@ -0,0 +1,14 @@
+﻿namespace Tienda_PrograVI.Models
+{
+    public class Producto
+    {
+       public int Id_producto { get; set; }
+       public string Nombre { get; set; }
+       public string Descripcion { get; set; }
+       public double Precio { get; set; }
+       public int Stock { get; set; }
+       public int  Id_Categoria {  get; set; }
+
+
+    }
+}
